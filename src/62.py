6df18350b2c24ddb025d33bd794d37bbd4ed92e4
bezhangurgul749 <bezhangurgul749@gmail.com>
2025-04-07 15:34:48 +0000

    Add 62.py

---
+++ src/62.py
@@ -0,0 +1,11 @@
+def find_max_in_list(lst):
+    """
+    Find the maximum value in a list of integers.
+    
+    Args:
+    lst (list): A list of integers.
+    
+    Returns:
+    int: The maximum value in the list.
+    """
+    return max(lst)
