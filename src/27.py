a84d9e971d3c6910c28466f7aa74579815045308
bezhangurgul749 <bezhangurgul749@gmail.com>
2025-04-07 14:47:01 +0000

    Add 27.py

---
+++ src/27.py
@@ -0,0 +1,14 @@
+def calculate_area(radius):
+    """
+    Calculate the area of a circle.
+    
+    Parameters:
+    radius (float): The radius of the circle.
+    
+    Returns:
+    float: The area of the circle.
+    """
+    area = 3.14 * radius ** 2
+    return area
+
+calculate_area(5)
