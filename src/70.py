46842995169abcabfb7f68197073cf9732407c1c
bezhangurgul749 <bezhangurgul749@gmail.com>
2025-04-07 15:47:38 +0000

    Add 70.py

---
+++ src/70.py
@@ -0,0 +1,10 @@
+def find_factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        factorial = 1
+        for i in range(2, n + 1):
+            factorial *= i
+        return factorial
+
+print(find_factorial(5))
