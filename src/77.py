ac97825dd58e23ae6fb89cd859f6807ac12eb1ba
bezhangurgul749 <bezhangurgul749@gmail.com>
2025-04-07 15:56:35 +0000

    Add 77.py

---
+++ src/77.py
@@ -0,0 +1,7 @@
+def find_largest_value(numbers):
+    largest_value = max(numbers)
+    return largest_value
+
+numbers = [4, 2, 5, 1, 3]
+largest_number = find_largest_value(numbers)
+print("The largest number is:", largest_number)
