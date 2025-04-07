e0d69d3ac0eab9382db34e9e086afca0b38caefb
bezhangurgul749 <bezhangurgul749@gmail.com>
2025-04-07 14:51:58 +0000

    Add 30.py

---
+++ src/30.py
@@ -0,0 +1,11 @@
+def find_factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        factorial = 1
+        for i in range(2, n + 1):
+            factorial *= i
+        return factorial
+
+# Example usage
+print(find_factorial(5))  # Output: 120
