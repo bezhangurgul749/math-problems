0baac20a8a3d73ba7c7f491bd7034344bd950224
bezhangurgul749 <bezhangurgul749@gmail.com>
2025-05-24 08:33:44 +0000

    Add 125.py

---
+++ src/125.py
@@ -0,0 +1,63 @@
+def add_numbers(x, y):
+    """
+    Add two numbers.
+    
+    Parameters:
+    x (int/float): The first number to add.
+    y (int/float): The second number to add.
+    
+    Returns:
+    int/float: The sum of the two numbers.
+    """
+    return x + y
+
+def subtract_numbers(x, y):
+    """
+    Subtract two numbers.
+    
+    Parameters:
+    x (int/float): The first number to subtract from.
+    y (int/float): The second number to subtract from.
+    
+    Returns:
+    int/float: The result of the subtraction.
+    """
+    return x - y
+
+def multiply_numbers(x, y):
+    """
+    Multiply two numbers.
+    
+    Parameters:
+    x (int/float): The first number to multiply.
+    y (int/float): The second number to multiply.
+    
+    Returns:
+    int/float: The product of the two numbers.
+    """
+    return x * y
+
+def divide_numbers(x, y):
+    """
+    Divide two numbers.
+    
+    Parameters:
+    x (int/float): The numerator of the division.
+    y (int/float): The denominator of the division.
+    
+    Returns:
+    int/float: The quotient of the division.
+    """
+    return x / y
+
+def square_numbers(x):
+    """
+    Square a number.
+    
+    Parameters:
+    x (int/float): The number to be squared.
+    
+    Returns:
+    int/float: The square of the given number.
+    """
+    return x ** 2
